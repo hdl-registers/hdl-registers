8398655fc8371eace17f52c2612713553e0b9eac
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-10-21 09:34:30 +0200

    Add tsfpga to PYTHONPATH when running tests

--- hdl_registers/test/conftest.py
+++ hdl_registers/test/conftest.py
@@ -6,5 +6,15 @@
 # https://gitlab.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
 
+import sys
+
+from pathlib import Path
+
+# Many tests use helper methods from tsfpga.
+# Do PYTHONPATH insert() instead of append() to prefer any local repo checkout over any pip install
+REPO_ROOT = Path(__file__).parent.parent.parent
+PATH_TO_TSFPGA = REPO_ROOT.parent.resolve() / "tsfpga"
+sys.path.insert(0, str(PATH_TO_TSFPGA))
+
 # pylint: disable=unused-import
 from tsfpga.test.conftest import fixture_tmp_path  # noqa: F401
