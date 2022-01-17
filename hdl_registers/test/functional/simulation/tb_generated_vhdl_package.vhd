d1fdce89a4aa75fb2a1189e566f7a979f0eb0a13
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2022-01-17 14:20:43 +0100

    Add test that simulates the generated VHDL package
    Closes #10

---
+++ hdl_registers/test/functional/simulation/tb_generated_vhdl_package.vhd
@@ -0,0 +1,86 @@
+-- -------------------------------------------------------------------------------------------------
+-- Copyright (c) Lukas Vik. All rights reserved.
+--
+-- This file is part of the hdl_registers project.
+-- https://hdl-registers.com
+-- https://gitlab.com/tsfpga/hdl_registers
+-- -------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+library reg_file;
+use reg_file.reg_file_pkg.all;
+
+use work.example_regs_pkg.all;
+
+
+entity tb_generated_vhdl_package is
+  generic (
+    runner_cfg : string
+  );
+end entity;
+
+architecture tb of tb_generated_vhdl_package is
+
+begin
+
+  ------------------------------------------------------------------------------
+  main : process
+  begin
+    test_runner_setup(runner, runner_cfg);
+
+    if run("test_generated_register_addresses") then
+      check_equal(example_configuration, 0);
+      check_equal(example_status, 1);
+
+      check_equal(example_base_addresses_array_length, 3);
+      check_equal(example_base_addresses_read_address(0), 2);
+      check_equal(example_base_addresses_write_address(0), 3);
+      check_equal(example_base_addresses_read_address(1), 4);
+      check_equal(example_base_addresses_write_address(1), 5);
+      check_equal(example_base_addresses_read_address(2), 6);
+      check_equal(example_base_addresses_write_address(2), 7);
+
+    elsif run("test_generated_register_modes") then
+      assert example_reg_map(example_configuration).reg_type = r_w;
+      assert example_reg_map(example_status).reg_type = r;
+
+      assert example_reg_map(example_base_addresses_read_address(0)).reg_type = w;
+      assert example_reg_map(example_base_addresses_write_address(0)).reg_type = w;
+      assert example_reg_map(example_base_addresses_read_address(1)).reg_type = w;
+      assert example_reg_map(example_base_addresses_write_address(1)).reg_type = w;
+      assert example_reg_map(example_base_addresses_read_address(2)).reg_type = w;
+      assert example_reg_map(example_base_addresses_write_address(2)).reg_type = w;
+
+    elsif run("test_generated_register_field_indexes") then
+      -- Generated bit field indexes should match the order and widths in the TOML
+
+      -- Status register
+      check_equal(example_status_idle, 0);
+      check_equal(example_status_stalling, 1);
+
+      check_equal(example_status_counter'low, 2);
+      check_equal(example_status_counter'high, 9);
+      check_equal(example_status_counter_width, 8);
+      check_equal(example_status_counter_t'high, 7);
+      check_equal(example_status_counter_t'low, 0);
+
+      -- Fields in array registers
+      check_equal(example_base_addresses_read_address_address'low, 0);
+      check_equal(example_base_addresses_read_address_address'high, 27);
+      check_equal(example_base_addresses_read_address_address_width, 28);
+
+      check_equal(example_base_addresses_write_address_address'low, 0);
+      check_equal(example_base_addresses_write_address_address'high, 27);
+      check_equal(example_base_addresses_write_address_address_width, 28);
+
+    end if;
+
+    test_runner_cleanup(runner);
+  end process;
+
+end architecture;
---
+++ hdl_registers/test/functional/simulation/test_generated_vhdl_package.py
@@ -0,0 +1,48 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl_registers project.
+# https://hdl-registers.com
+# https://gitlab.com/tsfpga/hdl_registers
+# --------------------------------------------------------------------------------------------------
+
+from pathlib import Path
+
+from vunit import VUnit
+
+from tsfpga.module import get_hdl_modules
+
+from hdl_registers import HDL_REGISTERS_DOC
+from hdl_registers.parser import from_toml
+
+THIS_FOLDER = Path(__file__).parent.resolve()
+
+
+def test_running_simulation(tmp_path):
+    """
+    Run the testbench .vhd file that is next to this file. Contains assertions on the
+    VHDL package generated from the example TOML file. Shows that the file can be compiled and
+    that (some of) the information is correct.
+    """
+    register_list = from_toml(
+        module_name="example", toml_file=HDL_REGISTERS_DOC / "sphinx" / "files" / "example.toml"
+    )
+    register_list.create_vhdl_package(output_path=tmp_path)
+
+    vunit_proj = VUnit.from_argv(
+        argv=["--minimal", "--num-threads", "4", "--output-path", str(tmp_path)]
+    )
+
+    library = vunit_proj.add_library(library_name="example")
+    library.add_source_file(THIS_FOLDER / "tb_generated_vhdl_package.vhd")
+    library.add_source_file(tmp_path / "example_regs_pkg.vhd")
+
+    for module in get_hdl_modules():
+        vunit_library = vunit_proj.add_library(library_name=module.library_name)
+        for hdl_file in module.get_simulation_files(include_tests=False):
+            vunit_library.add_source_file(hdl_file.path)
+
+    try:
+        vunit_proj.main()
+    except SystemExit as exception:
+        assert exception.code == 0
