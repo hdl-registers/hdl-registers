6e6fd28966655c0f331806fe983fc19e893a37f4
lukas <lukas@lukas-pc>
2023-10-11 15:35:34 +0200

    Set pre-release version number

--- hdl_registers/__init__.py
+++ hdl_registers/__init__.py
@@ -20,5 +20,5 @@ HDL_REGISTERS_TEST = HDL_REGISTERS_PATH / "test"
 HDL_REGISTERS_DOC = REPO_ROOT / "doc"
 HDL_REGISTERS_GENERATED = REPO_ROOT / "generated"
 
-__version__ = "4.0.1"
+__version__ = "4.0.2-dev"
 __doc__ = get_short_slogan()  # pylint: disable=redefined-builtin
