6ce6ad7c346592c75a2dc9972d936141fe5c6799
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-01-18 10:43:49 +0100

    Release version 7.0.0

--- hdl_registers/__init__.py
+++ hdl_registers/__init__.py
@@ -21,5 +21,5 @@ HDL_REGISTERS_GENERATED = REPO_ROOT / "generated"
 HDL_REGISTERS_TESTS = REPO_ROOT / "tests"
 HDL_REGISTERS_TOOLS = REPO_ROOT / "tools"
 
-__version__ = "6.2.1-dev3"
+__version__ = "7.0.0"
 __doc__ = get_short_slogan()  # pylint: disable=redefined-builtin
